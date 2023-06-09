00d083aa06fe36b0e4719c45d7a085d5ad5a90c6
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-09 15:14:12 +0100

    Update PatientVO.kt

--- app/src/main/java/com/example/patient/PatientVO.kt
+++ app/src/main/java/com/example/patient/PatientVO.kt
@@ -4,11 +4,13 @@ import java.util.ArrayList
 
 class PatientVO  {
 
-    private var patientId: String = ""
-    private var name: String = ""
-    private var appointmentId: String = ""
+     var patientId: String = ""
+     var name: String = ""
+     var appointmentId: String = ""
 
-    constructor() {}
+    constructor() {
+    	//constructor
+    }
 
     constructor(patientIdx: String, 
         namex: String, 
@@ -19,10 +21,10 @@ class PatientVO  {
         this.appointmentId = appointmentIdx
     }
 
-    constructor (_x: Patient) {
-        patientId = _x.patientId
-        name = _x.name
-        appointmentId = _x.appointmentId
+    constructor (x: Patient) {
+        patientId = x.patientId
+        name = x.name
+        appointmentId = x.appointmentId
     }
 
     override fun toString(): String {
@@ -35,31 +37,6 @@ class PatientVO  {
             res.add(list[i].toString())
         }
         return res
-    }
-    
-    fun getPatientId(): String {
-        return patientId
-    }
-    
-    fun getName(): String {
-        return name
-    }
-    
-    fun getAppointmentId(): String {
-        return appointmentId
-    }
-    
-
-    fun setPatientId(_x: String) {
-    	patientId = _x
-    }
-    
-    fun setName(_x: String) {
-    	name = _x
-    }
-    
-    fun setAppointmentId(_x: String) {
-    	appointmentId = _x
     }
     
 }
