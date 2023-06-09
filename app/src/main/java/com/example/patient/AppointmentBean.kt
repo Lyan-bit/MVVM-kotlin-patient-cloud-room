81d109e2902427e6479028345022d82e3cf932a4
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-09 15:49:04 +0100

    Update AppointmentBean.kt

--- app/src/main/java/com/example/patient/AppointmentBean.kt
+++ app/src/main/java/com/example/patient/AppointmentBean.kt
@@ -45,7 +45,7 @@ class AppointmentBean(_c: Context) {
 		return errors.isNotEmpty()
 	}
 
-	suspend fun createAppointment() {
+	fun createAppointment() {
 		model.createAppointment(AppointmentEntity(appointmentId, code))
 		resetData()
 	}
@@ -80,7 +80,7 @@ class AppointmentBean(_c: Context) {
 		return errors.isNotEmpty()
 	}
 
-	suspend fun editAppointment() {
+	fun editAppointment() {
 		model.editAppointment(AppointmentEntity(appointmentId, code))
 		resetData()
 	}
@@ -93,7 +93,7 @@ class AppointmentBean(_c: Context) {
 		return errors.isNotEmpty()
 	}
 
-	suspend fun deleteAppointment() {
+	fun deleteAppointment() {
 		model.deleteAppointment(appointmentId)
 		resetData()
 	}
