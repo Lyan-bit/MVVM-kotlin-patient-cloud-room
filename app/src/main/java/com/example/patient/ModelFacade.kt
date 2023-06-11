c8adf55486d731613d7ac6c35315190e9a34d71b
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 21:00:00 +0100

    Update ModelFacade.kt

--- app/src/main/java/com/example/patient/ModelFacade.kt
+++ app/src/main/java/com/example/patient/ModelFacade.kt
@@ -1,123 +1,161 @@
 package com.example.patient
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import java.util.ArrayList
-
 
 class ModelFacade private constructor(context: Context) {
 
     private var cdb: FirebaseDB = FirebaseDB.getInstance()
-    private val db: DB by lazy { DB(context, null) } 
     private val fileSystem: FileAccessor by lazy { FileAccessor(context) }
+
 
     private var currentPatient: PatientVO? = null
     private var currentPatients: ArrayList<PatientVO> = ArrayList()
-    private var currentAppointment: AppointmentVO? = null
-	private var currentAppointments: ArrayList<AppointmentVO> = ArrayList()
 
     init {
-    	//init
-	}
+        //init
+    }
 
     companion object {
+        private val repository by lazy { Repository() }
         private var instance: ModelFacade? = null
         fun getInstance(context: Context): ModelFacade {
             return instance ?: ModelFacade(context)
         }
     }
-    
-    fun createPatient(x: PatientVO) { 
-			  editPatient(x)
-	}
-				    
+
+    val allAppointments: LiveData<List<AppointmentEntity>> = repository.allAppointments.asLiveData()
+
+    val allAppointmentAppointmentIds: LiveData<List<String>> = repository.allAppointmentappointmentIds.asLiveData()
+    val allAppointmentCodes: LiveData<List<String>> = repository.allAppointmentcodes.asLiveData()
+    private var currentAppointment: AppointmentEntity? = null
+    private var currentAppointments: List<AppointmentEntity> = ArrayList()
+
+    fun searchByAppointmentappointmentId(searchQuery: String): LiveData<List<AppointmentEntity>>  {
+        return repository.searchByAppointmentappointmentId(searchQuery).asLiveData()
+    }
+
+    fun searchByAppointmentcode(searchQuery: String): LiveData<List<AppointmentEntity>>  {
+        return repository.searchByAppointmentcode(searchQuery).asLiveData()
+    }
+
+
+    fun getAppointmentByPK(value: String): Flow<Appointment> {
+        val res: Flow<List<AppointmentEntity>> = repository.searchByAppointmentappointmentId(value)
+        return res.map { appointment ->
+            val itemx = Appointment.createByPKAppointment(value)
+            if (appointment.isNotEmpty()) {
+                itemx.appointmentId = appointment[0].appointmentId
+                itemx.code = appointment[0].code
+            }
+            itemx
+        }
+    }
+
+    fun createPatient(x: PatientVO) {
+        editPatient(x)
+    }
+
     fun editPatient(x: PatientVO) {
-		var obj = getPatientByPK(x.patientId)
-		if (obj == null) {
-			obj = Patient.createByPKPatient(x.patientId)
-		}
-			
-		  obj.patientId = x.patientId
-		  obj.name = x.name
-		  obj.appointmentId = x.appointmentId
-		cdb.persistPatient(obj)
-		currentPatient = x
-	}
-		
-	fun deletePatient(id: String) {
-			  val obj = getPatientByPK(id)
-			  if (obj != null) {
-			      cdb.deletePatient(obj)
-			          Patient.killPatient(id)
-			      }
-			  currentPatient = null	
-		}
-				    
+        var obj = getPatientByPK(x.patientId)
+        if (obj == null) {
+            obj = Patient.createByPKPatient(x.patientId)
+        }
+
+        obj.patientId = x.patientId
+        obj.name = x.name
+        obj.appointmentId = x.appointmentId
+        cdb.persistPatient(obj)
+        currentPatient = x
+    }
+
+    fun searchPatientById(search: String) : PatientVO {
+        var res = PatientVO()
+        for (x in currentPatients.indices) {
+            if ( currentPatients[x].patientId.toString() == search)
+                res = currentPatients[x]
+        }
+        return res
+    }
+
+    fun deletePatient(id: String) {
+        val obj = getPatientByPK(id)
+        if (obj != null) {
+            cdb.deletePatient(obj)
+            Patient.killPatient(id)
+        }
+        currentPatient = null
+    }
+
     fun setSelectedPatient(x: PatientVO) {
-			  currentPatient = x
-	}
-    fun createAppointment(x: AppointmentVO) { 
-          db.createAppointment(x)
-          currentAppointment = x
-	}
-		    
-    fun setSelectedAppointment(x: AppointmentVO) {
-	      currentAppointment = x
-	}
-	    
-    fun editAppointment(x: AppointmentVO) {
-        db.editAppointment(x)
-          currentAppointment = x
-	}	
-		
-    fun deleteAppointment(id: String) {
-          db.deleteAppointment(id)
-          currentAppointment = null
-	}
-		
+        currentPatient = x
+    }
+
+    suspend fun createAppointment(x: AppointmentEntity) {
+        repository.createAppointment(x)
+        currentAppointment = x
+    }
+
+    suspend fun editAppointment(x: AppointmentEntity) {
+        repository.updateAppointment(x)
+        currentAppointment = x
+    }
+
+    fun setSelectedAppointment(x: AppointmentEntity) {
+        currentAppointment = x
+    }
+
+    suspend fun deleteAppointment(id: String) {
+        repository.deleteAppointment(id)
+        currentAppointment = null
+    }
+
     fun addPatientattendsAppointment(appointmentId: String, patientId: String) {
-	      var obj = getPatientByPK(patientId)
-	      if (obj == null) {
-	          obj = Patient.createByPKPatient(patientId)
-          }
-	      obj.appointmentId = appointmentId
-	      cdb.persistPatient(obj)
-	      currentPatient = PatientVO(obj)
-	          
-	 }
-	    
+        var obj = getPatientByPK(patientId)
+        if (obj == null) {
+            obj = Patient.createByPKPatient(patientId)
+        }
+        obj.appointmentId = appointmentId
+        cdb.persistPatient(obj)
+        currentPatient = PatientVO(obj)
+
+    }
+
     fun removePatientattendsAppointment(appointmentId: String, patientId: String) {
-		     var obj = getPatientByPK(patientId)
-		     if (obj == null) {
-	             obj = Patient.createByPKPatient(patientId)
-	         }
-		     obj.appointmentId = "Null"
-		     cdb.persistPatient(obj)
-	         currentPatient = PatientVO(obj)
-		          
-	}
-	
+        var obj = getPatientByPK(patientId)
+        if (obj == null) {
+            obj = Patient.createByPKPatient(patientId)
+        }
+        obj.appointmentId = "Null"
+        cdb.persistPatient(obj)
+        currentPatient = PatientVO(obj)
 
-	fun listAppointment(): ArrayList<AppointmentVO> {
-        currentAppointments = db.listAppointment()
-		
+    }
+
+    suspend fun listAppointment(): List<AppointmentEntity> {
+        currentAppointments = repository.listAppointment()
         return currentAppointments
-	}
+    }
 
-	fun listAllAppointment(): ArrayList<Appointment> {	
-		currentAppointments = db.listAppointment()
-		var res = ArrayList<Appointment>()
-			for (x in currentAppointments.indices) {
-					val vo: AppointmentVO = currentAppointments[x]
-				    val itemx = Appointment.createByPKAppointment(vo.appointmentId)
-	            itemx.appointmentId = vo.appointmentId
+    suspend fun listAllAppointment(): ArrayList<Appointment> {
+        currentAppointments = repository.listAppointment()
+        var res = ArrayList<Appointment>()
+        for (x in currentAppointments.indices) {
+            val vo: AppointmentEntity = currentAppointments[x]
+            val itemx = Appointment.createByPKAppointment(vo.appointmentId)
+            itemx.appointmentId = vo.appointmentId
             itemx.code = vo.code
-			res.add(itemx)
-		}
-		return res
-	}
+            res.add(itemx)
+        }
+        return res
+    }
 
-    fun stringListAppointment(): ArrayList<String> {
-        currentAppointments = db.listAppointment()
+    suspend fun stringListAppointment(): List<String> {
+        currentAppointments = repository.listAppointment()
         val res: ArrayList<String> = ArrayList()
         for (x in currentAppointments.indices) {
             res.add(currentAppointments[x].toString())
@@ -125,29 +163,29 @@ class ModelFacade private constructor(context: Context) {
         return res
     }
 
-    fun getAppointmentByPK(value: String): Appointment? {
-        val res: ArrayList<AppointmentVO> = db.searchByAppointmentappointmentId(value)
-	        return if (res.isEmpty()) {
-	            null
-	        } else {
-	            val vo: AppointmentVO = res[0]
-	            val itemx = Appointment.createByPKAppointment(value)
+    suspend fun getAppointmentByPK2(value: String): Appointment? {
+        val res: List<AppointmentEntity> = repository.searchByAppointmentappointmentId2(value)
+        return if (res.isEmpty()) {
+            null
+        } else {
+            val vo: AppointmentEntity = res[0]
+            val itemx = Appointment.createByPKAppointment(value)
             itemx.appointmentId = vo.appointmentId
             itemx.code = vo.code
-	            itemx
-	        }
-    }
-    
-    fun retrieveAppointment(value: String): Appointment? {
-        return getAppointmentByPK(value)
+            itemx
+        }
     }
 
-    fun allAppointmentAppointmentIds(): ArrayList<String> {
-        currentAppointments = db.listAppointment()
+    suspend fun retrieveAppointment(value: String): Appointment? {
+        return getAppointmentByPK2(value)
+    }
+
+    suspend fun allAppointmentAppointmentIds(): ArrayList<String> {
+        currentAppointments = repository.listAppointment()
         val res: ArrayList<String> = ArrayList()
-            for (appointment in currentAppointments.indices) {
-                res.add(currentAppointments[appointment].appointmentId)
-            }
+        for (appointment in currentAppointments.indices) {
+            res.add(currentAppointments[appointment].appointmentId)
+        }
         return res
     }
 
@@ -157,35 +195,33 @@ class ModelFacade private constructor(context: Context) {
         }
     }
 
-    fun getSelectedAppointment(): AppointmentVO? {
+    fun getSelectedAppointment(): AppointmentEntity? {
         return currentAppointment
     }
 
-    fun persistAppointment(x: Appointment) {
-        val vo = AppointmentVO(x)
-        db.editAppointment(vo)
+    suspend fun persistAppointment(x: Appointment) {
+        val vo = AppointmentEntity(x.appointmentId, x.code)
+        repository.updateAppointment(vo)
         currentAppointment = vo
     }
-	
 
-	    	fun listPatient(): ArrayList<PatientVO> {
-		  val patients: ArrayList<Patient> = Patient.patientAllInstances
-		  currentPatients.clear()
-		  for (i in patients.indices) {
-		       currentPatients.add(PatientVO(patients[i]))
-		  }
-			      
-		 return currentPatients
-	}
-	
-	fun listAllPatient(): ArrayList<Patient> {
-		  val patients: ArrayList<Patient> = Patient.patientAllInstances    
-		  return patients
-	}
-	
 
-			    
-    fun stringListPatient(): ArrayList<String> {
+    fun listPatient(): List<PatientVO> {
+        val patients: ArrayList<Patient> = Patient.patientAllInstances
+        currentPatients.clear()
+        for (i in patients.indices) {
+            currentPatients.add(PatientVO(patients[i]))
+        }
+
+        return currentPatients
+    }
+
+    fun listAllPatient(): ArrayList<Patient> {
+        val patients: ArrayList<Patient> = Patient.patientAllInstances
+        return patients
+    }
+
+    fun stringListPatient(): List<String> {
         val res: ArrayList<String> = ArrayList()
         for (x in currentPatients.indices) {
             res.add(currentPatients[x].toString())
@@ -196,19 +232,19 @@ class ModelFacade private constructor(context: Context) {
     fun getPatientByPK(value: String): Patient? {
         return Patient.patientIndex[value]
     }
-    
+
     fun retrievePatient(value: String): Patient? {
-            return getPatientByPK(value)
+        return getPatientByPK(value)
     }
 
     fun allPatientPatientIds(): ArrayList<String> {
         val res: ArrayList<String> = ArrayList()
-            for (x in currentPatients.indices) {
-                res.add(currentPatients[x].patientId)
-            }
+        for (x in currentPatients.indices) {
+            res.add(currentPatients[x].patientId)
+        }
         return res
     }
-    
+
     fun setSelectedPatient(i: Int) {
         if (i < currentPatients.size) {
             currentPatient = currentPatients[i]
@@ -225,5 +261,4 @@ class ModelFacade private constructor(context: Context) {
         currentPatient = vo
     }
 
-	
 }
